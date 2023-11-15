eba5b7a32466f5e68ea8f7b8b32ae5e69afcb8c9
Santos Hernandez <santos@zebedee.io>
2023-11-14 21:29:46 -0700

    LFG

--- components/Items.js
+++ components/Items.js
@@ -7,9 +7,12 @@ const Items = ({ items }) => {
       <h3 className="mb-4 text-lg font-semibold">{items.title}</h3>
       <div className=" my-6 w-auto">
         {items.data.map((item) => (
-          <div key={item[1]} className="border-2 divide-y divide-slate-700">
+          <div
+            key={item[1]}
+            className="border-2 divide-y divide-slate-700 rounded"
+          >
             <Link className="hover:text-lime-400 my-4 text-base" href={item[1]}>
-              {item[0]}
+              <p className="text-base m-2">{item[0]}</p>
             </Link>
           </div>
         ))}
